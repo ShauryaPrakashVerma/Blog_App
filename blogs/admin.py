b39a97877551f55da1466baec7b5129168baa675
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-06-16 02:13:58 +0530

    Create Blog model

---
+++ blogs/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Category, Blog
+
+# prepopulated fields for generating slug sutomatically
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug' : ('title',)}
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')  # display additional info about the models specified in the tuple
+    search_fields = ('id', 'title', 'category__category_name', 'status')  # what all parameters we can seach by, we cannot use a foreign field for search
+    
+    # to use foreign key as a seach parameter you must tell it which field of the related model to search.  category --> category__category_name
+
+    list_editable = ('is_featured',)  # makes the is_feature option editable from top without opening
+
+# by default the models created are not visible in the admin panel, we have first register the model in the admin.py
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
+
---
+++ blogs/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    
+    # by default django makes the name plural by adding 's' after the name of the model so 'Category' becomes 'Categorys'
+    # This behaviur can be fixed by the Meta class
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+        
+    # what should be the category actually be represented as in the admin panel
+    def __str__(self):
+        return self.category_name
+    
+    
+STATUS_CHOICES = (
+    ("Draft", "Draft"),
+    ("Published", "Published")
+)    
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)   # models.CASCADE ensures that if the user is deleted all his posts are also deleted
+    featured_image = models.ImageField(upload_to='uploads/%Y/%m/%d')
+    short_description = models.TextField(max_length=500)
+    blog_body = models.TextField(max_length=2000)
+    status = models.CharField(max_length=20 ,choices=STATUS_CHOICES, default="Draft")   # to create a dropdown
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+ 
+ 
+ 
+   
