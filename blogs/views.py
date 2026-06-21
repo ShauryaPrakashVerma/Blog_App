301fc9f8e26f6644f737ac61495e9ad480d4a526
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-06-22 00:56:53 +0530

    Complete Search feature

--- blogs/views.py
+++ blogs/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Blog, Category
+from django.db.models import Q
 
 # Create your views here.
 def posts_by_category(request, category_id):
@@ -38,4 +39,10 @@ def blogs(request, slug):
 
 
 def search(request):
-    return render(request, "search.html")
+    keyword = request.GET.get('keyword')
+    blogs = Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains = keyword) | Q(blog_body__icontains=keyword), status = "Published")
+    context = {
+        "blogs" : blogs,
+        "keyword" : keyword
+    }
+    return render(request, "search.html", context = context)
