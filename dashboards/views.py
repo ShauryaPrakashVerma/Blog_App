9c87ae9c51a5b43c89d460da2683110b9f49ea51
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-06-24 01:20:48 +0530

    Category CRUD functionality

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
+
+from .forms import CategoryForm
 
 
 @login_required(login_url='login')
@@ -18,3 +20,39 @@ def categories(request):
 
 def posts(request):
     return render(request, "dashboard/posts.html")
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+        
+    form = CategoryForm()
+    context = {
+        'form' : form
+    }
+    return render(request, 'dashboard/add_category.html', context=context)
+
+
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+    
+    form = CategoryForm(instance=category)
+    context = {
+        'form' : form,
+        'category' : category
+    }
+    return render(request, 'dashboard/edit_category.html', context=context)
+
+
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    return redirect('categories')
