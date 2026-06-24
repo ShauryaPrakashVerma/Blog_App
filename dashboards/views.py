1b602915afbac1749ae309dd71cb1b24c4131fa2
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-06-25 01:44:18 +0530

    Complete delete posts

--- dashboards/views.py
+++ dashboards/views.py
@@ -3,6 +3,7 @@ from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
 
 from .forms import CategoryForm, BlogForm
+from django.template.defaultfilters import slugify
 
 
 @login_required(login_url='login')
@@ -75,6 +76,9 @@ def add_posts(request):
             post = form.save(commit=False)  # temporary save and returns object, now we can add custom data
             post.author = request.user
             post.save()
+            title = form.cleaned_data['title']
+            post.slug = slugify(title) + '-' + str(post.id)  # new post can be repeated so to avoid this we use pk at end of the slug
+            post.save()
             return redirect('posts')
         else:
             print("Form Invalid")
@@ -84,3 +88,29 @@ def add_posts(request):
         'form' : form
     }
     return render(request, 'dashboard/add_posts.html', context)
+
+
+def edit_posts(request, pk):
+    post = get_object_or_404(Blog, pk=pk)
+    
+    if request.method == "POST":
+        form = BlogForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            title = form.cleaned_data['title']
+            post.slug = slugify(title) + '-' + str(post.id)
+            post.save()
+            return redirect('posts')
+    form = BlogForm(instance=post)
+    context = {
+        'form' : form,
+        'post' : post
+    }
+    
+    return render(request, 'dashboard/edit_posts.html', context=context)
+
+
+def delete_posts(reuqest, pk):
+    post = get_object_or_404(Blog , pk=pk)
+    post.delete()
+    return redirect('posts')
